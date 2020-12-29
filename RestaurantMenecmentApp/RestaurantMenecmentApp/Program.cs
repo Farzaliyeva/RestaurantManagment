e599c55bdd1819183e47676bfb0190b9dda82fe0
unknown <Administrator@CADR12.code.local>
2020-12-29 17:55:11 +0400

    project started

---
+++ RestaurantMenecmentApp/RestaurantMenecmentApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestaurantMenecmentApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
